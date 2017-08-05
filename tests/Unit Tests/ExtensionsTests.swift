159ac9573065efb2f7941142ea32e44411597a31
Vinicius Vendramini <vinivendra@gmail.com>
2017-08-05 17:31:20 -0300

    Added tests for the shift method.

---
+++ tests/Unit Tests/ExtensionsTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+class ExtensionsTests: XCTestCase {
+	func testShift() {
+		let empty = Array([0].dropFirst())
+		XCTAssertEqual(empty, empty.shifted())
+		XCTAssertEqual([1], [1].shifted())
+		XCTAssertEqual([2, 1], [1, 2].shifted())
+		XCTAssertEqual([2, 3, 1], [1, 2, 3].shifted())
+		XCTAssertEqual([2, 3, 4, 5, 1], [1, 2, 3, 4, 5].shifted())
+
+		// Ensure `shift` is the same as `shifted`
+		var original = [1, 2, 3, 4, 5]
+		let copy = original
+		original.shift()
+		XCTAssertEqual(original, copy.shifted())
+	}
+}
