14ebfb088dc93bbd43f632d1b427be670d811dc8
Vinicius Vendramini <vinivendra@gmail.com>
2017-08-17 10:38:20 -0300

    Refactored Lexer tests to fit new testing styles.

--- tests/Unit Tests/Lexer Tests/LexerTests.swift
+++ tests/Unit Tests/Lexer Tests/LexerTests.swift
@@ -7,32 +7,52 @@ import XCTest
 private let testFilesPath = CommandLine.arguments[1] +
 "/tests/Unit Tests/Lexer Tests/"
 
+extension Array where Element == Token {
+	func filter(byType type: Int) -> [Token] {
+		return self.filter { $0.getType() == type }
+	}
+
+	func countTokens(ofType type: Int) -> Int {
+		return self.filter(byType: type).count
+	}
+}
+
 class LexerTests: XCTestCase {
-	func testIDs() {
-		let contents = try! String(contentsOfFile: testFilesPath + "TestIDs")
-
-		let inputStream = ANTLRInputStream(contents)
-		let lexer = JokrLexer(inputStream)
-
+	func getTokens(inFile filename: String) throws -> [Token] {
 		do {
-			let tokens = try lexer.getAllTokens()
-			let ids: [(line: Int, charInLine: Int, text: String)] =
+			let contents = try! String(contentsOfFile: testFilesPath + filename)
+			let inputStream = ANTLRInputStream(contents)
+			let lexer = JokrLexer(inputStream)
+			return try lexer.getAllTokens()
+		} catch (let error) {
+			throw error
+		}
+	}
+
+	func testIDs() {
+		do {
+			// WITH:
+			let tokens = try getTokens(inFile: "TestIDs")
+
+			let expectedTokens: [(line: Int, charInLine: Int, text: String)] =
 				[(4, 0, "foo"), (4, 4, "bar"), (5, 0, "baz"), (6, 0, "blah"),
 				 (7, 0, "b0lah"), (8, 0, "b0l0ah"), (9, 0, "blah0"),
 				 (10, 0, "_lah"), (11, 0, "__lah"), (12, 0, "_lah0"),
 				 (13, 0, "_la0h0")]
 
-			for id in ids {
+			// TEST: Lexer found all expected tokens (in order)
+			for expected in expectedTokens {
 				XCTAssert(tokens.contains { token in
 					return token.getType() == JokrLexer.ID
-						&& token.getLine() == id.line
-						&& token.getCharPositionInLine() == id.charInLine
-						&& token.getText() == id.text
-				}, "Token not found: \(id)")
+						&& token.getLine() == expected.line
+						&& token.getCharPositionInLine() == expected.charInLine
+						&& token.getText() == expected.text
+				}, "Token not found: \(expected)")
 			}
 
-			XCTAssertEqual(tokens.filter { $0.getType() == JokrLexer.ID }.count,
-			               ids.count)
+			// TEST: Lexer didn't find any other tokens of this type
+			XCTAssertEqual(tokens.countTokens(ofType: JokrLexer.ID),
+			               expectedTokens.count)
 		}
 		catch (let error) {
 			XCTFail("JokrLexer failed to get tokens.\nError: \(error)")
@@ -40,31 +60,29 @@ class LexerTests: XCTestCase {
 	}
 
 	func testTypes() {
-		let contents = try! String(contentsOfFile: testFilesPath + "TestTypes")
-
-		let inputStream = ANTLRInputStream(contents)
-		let lexer = JokrLexer(inputStream)
-
 		do {
-			let tokens = try lexer.getAllTokens()
-			let types: [(line: Int, charInLine: Int, text: String)] =
+			// WITH:
+			let tokens = try getTokens(inFile: "TestTypes")
+
+			let expectedTokens: [(line: Int, charInLine: Int, text: String)] =
 				[(4, 0, "Foo"), (4, 4, "Bar"), (5, 0, "Baz"), (6, 0, "Blah"),
 				 (7, 0, "B0lah"), (8, 0, "B0l0ah"), (9, 0, "Blah0"),
 				 (10, 0, "_Lah"), (11, 0, "__Lah"), (12, 0, "_Lah0"),
 				 (13, 0, "_La0h0")]
 
-			for type in types {
+			// TEST: Lexer found all expected tokens (in order)
+			for expected in expectedTokens {
 				XCTAssert(tokens.contains { token in
 					return token.getType() == JokrLexer.TYPE
-						&& token.getLine() == type.line
-						&& token.getCharPositionInLine() == type.charInLine
-						&& token.getText() == type.text
-				}, "Token not found: \(type)")
+						&& token.getLine() == expected.line
+						&& token.getCharPositionInLine() == expected.charInLine
+						&& token.getText() == expected.text
+				}, "Token not found: \(expected)")
 			}
 
-			XCTAssertEqual(
-				tokens.filter { $0.getType() == JokrLexer.TYPE }.count,
-				types.count)
+			// TEST: Lexer didn't find any other tokens of this type
+			XCTAssertEqual(tokens.countTokens(ofType: JokrLexer.TYPE),
+			               expectedTokens.count)
 		}
 		catch (let error) {
 			XCTFail("JokrLexer failed to get tokens.\nError: \(error)")
@@ -72,18 +90,9 @@ class LexerTests: XCTestCase {
 	}
 
 	func testComments() {
-		// Ensure there are 5 comments in the file,
-		// that id's in the same lines as comments get recognized,
-		// that comments don't eat newlines (i.e. there are 5 newline tokens)
-
-		let contents = try! String(contentsOfFile:
-			testFilesPath + "TestComments")
-
-		let inputStream = ANTLRInputStream(contents)
-		let lexer = JokrLexer(inputStream)
-
 		do {
-			let tokens = try lexer.getAllTokens()
+			// WITH:
+			let tokens = try getTokens(inFile: "TestComments")
 
 			let expectedTokens:
 				[(line: Int, charInLine: Int, length: Int, type: Int)] =
@@ -97,22 +106,23 @@ class LexerTests: XCTestCase {
 				 (5, 0, 3, JokrLexer.ID),
 				 (5, 47, 3, JokrLexer.ID)]
 
-			for aToken in expectedTokens {
+			// TEST: Lexer found all expected tokens (in order)
+			for expected in expectedTokens {
 				XCTAssert(tokens.contains { token in
-					return token.getType() == aToken.type
-						&& token.getLine() == aToken.line
-						&& token.getCharPositionInLine() == aToken.charInLine
-						&& token.getText()!.characters.count == aToken.length
-				}, "Token not found: \(aToken)")
+					return token.getType() == expected.type
+						&& token.getLine() == expected.line
+						&& token.getCharPositionInLine() == expected.charInLine
+						&& token.getText()!.characters.count == expected.length
+				}, "Token not found: \(expected)")
 			}
 
-			XCTAssertEqual(tokens.filter {
-				$0.getType() == JokrLexer.BLOCK_COMMENT
-					|| $0.getType() == JokrLexer.LINE_COMMENT
-				}.count, 5)
-			XCTAssertEqual(tokens.filter {
-				$0.getType() == JokrLexer.NEW_LINE
-				}.count, 5)
+			// TEST: Lexer didn't find any extra comment tokens
+			XCTAssertEqual(
+				tokens.countTokens(ofType: JokrLexer.BLOCK_COMMENT) +
+					tokens.countTokens(ofType: JokrLexer.LINE_COMMENT), 5)
+
+			// TEST: Comments didn't eat any newlines
+			XCTAssertEqual(tokens.countTokens(ofType: JokrLexer.NEW_LINE), 5)
 
 		}
 		catch (let error) {
@@ -121,29 +131,27 @@ class LexerTests: XCTestCase {
 	}
 
 	func testNewlines() {
-		let contents = try! String(contentsOfFile: testFilesPath + "TestNewlines")
-
-		let inputStream = ANTLRInputStream(contents)
-		let lexer = JokrLexer(inputStream)
-
 		do {
-			let tokens = try lexer.getAllTokens()
-			let newlines: [(line: Int, charInLine: Int)] =
+			// WITH:
+			let tokens = try getTokens(inFile: "TestNewlines")
+
+			let expectedTokens: [(line: Int, charInLine: Int)] =
 				[(1, 59), (2, 37), (3, 25), (4, 46), (5, 50), (6, 7), (7, 3),
 				 (8, 4), (9, 5), (10, 7), (11, 5), (12, 14), (13, 8), (14, 5),
 				 (15, 8)]
 
-			for newline in newlines {
+			// TEST: Lexer found all expected tokens (in order)
+			for expected in expectedTokens {
 				XCTAssert(tokens.contains { token in
 					return token.getType() == JokrLexer.NEW_LINE
-						&& token.getLine() == newline.line
-						&& token.getCharPositionInLine() == newline.charInLine
-				}, "Token not found: \(newline)")
+						&& token.getLine() == expected.line
+						&& token.getCharPositionInLine() == expected.charInLine
+				}, "Token not found: \(expected)")
 			}
 
-			XCTAssertEqual(tokens.filter {
-				$0.getType() == JokrLexer.NEW_LINE
-				}.count, newlines.count)
+			// TEST: Lexer didn't find any other tokens of this type
+			XCTAssertEqual(tokens.countTokens(ofType: JokrLexer.NEW_LINE),
+			               expectedTokens.count)
 		}
 		catch (let error) {
 			XCTFail("JokrLexer failed to get tokens.\nError: \(error)")
