6be4f89817f2f210c35f228fe72cfb1897b7c63e
Vinicius Vendramini <vinivendra@gmail.com>
2017-08-17 16:34:58 -0300

    Added TODO's to guide future development.

--- jokr/main.swift
+++ jokr/main.swift
@@ -1,6 +1,9 @@
 import Foundation
 import Antlr4
 
+// TODO: Add tests for Java translator and ObjC translator
+// TODO: Add integration/acceptance tests for Java and ObjC
+
 private let filePath = CommandLine.arguments[1] + "/tests/"
 
 do {
