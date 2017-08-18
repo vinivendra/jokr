b874aabbc7e711d603be7e7ec18be99cfe3e171a
Vinicius Vendramini <vinivendra@gmail.com>
2017-08-18 10:47:39 -0300

    Added test for assignments.

--- tests/Unit Tests/Java Translator Tests/Java Translator Tests.swift
+++ tests/Unit Tests/Java Translator Tests/Java Translator Tests.swift
@@ -7,10 +7,13 @@ private let testFilesPath = CommandLine.arguments[1] +
 private let errorMessage =
 	"Lexer, Parser or Translator failed during test.\nError: "
 
+////////////////////////////////////////////////////////////////////////////////
 // swiftlint:disable line_length
 let emptyMainContents = "public class Main {\n\tpublic static void main(String []args) {\n\t}\n}\n"
+let assignmentMainContents = "public class Main {\n\tpublic static void main(String []args) {\n\t\tint x = 2;\n\t\tint y = x + x;\n\t\tfloat z = y - x;\n\t}\n}\n"
 // swiftlint:enable line_length
 
+////////////////////////////////////////////////////////////////////////////////
 class JavaTranslatorTests: XCTestCase {
 	let parser = JKRAntlrParser()
 
@@ -21,6 +24,7 @@ class JavaTranslatorTests: XCTestCase {
 			let writer = JKRStringWriter()
 			let translator = JKRJavaTranslator(writingWith: writer)
 			try translator.translate(program: program)
+			writer.prettyPrint()
 			return writer.files
 		}
 		catch (let error) {
@@ -38,6 +42,22 @@ class JavaTranslatorTests: XCTestCase {
 
 			// TEST: Empty main file gets created
 			XCTAssertEqual(files["Main.java"], emptyMainContents)
+
+			// TEST: No other files get created
+			XCTAssertEqual(files.count, 1)
+		}
+		catch (let error) {
+			XCTFail(errorMessage + "\(error)")
+		}
+	}
+
+	func testAssignments() {
+		do {
+			// WITH:
+			let files = try translate(file: "TestAssignments.jkr")
+
+			// TEST: Empty main file gets created
+			XCTAssertEqual(files["Main.java"], assignmentMainContents)
 
 			// TEST: No other files get created
 			XCTAssertEqual(files.count, 1)
