45806b41f474f793cbdc86dc8caa062bcd062d83
Vinicius Vendramini <vinivendra@gmail.com>
2017-08-30 15:37:07 -0300

    Updated TODO's.

--- jokr/main.swift
+++ jokr/main.swift
@@ -1,10 +1,6 @@
 import Foundation
 import Antlr4
 
-// Branch - acceptance tests
-// TODO: Add acceptance tests for expressions and assignments
-// TODO: Consider adding Strings to allow print formatting with any types
-
 // Branch - declarations
 // TODO: Add class declarations
 // TODO: Integrate class declarations to output file structure
