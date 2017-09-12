d947762acfec6f9169fa6b3e7973a45490cbe8bd
Vinicius Vendramini <vinivendra@gmail.com>
2017-09-12 08:55:46 -0300

    Updated TODO's

--- jokr/main.swift
+++ jokr/main.swift
@@ -2,10 +2,11 @@ import Foundation
 import Antlr4
 
 // Branch - ClassDeclarations
-// TODO: Add class declarations
-// TODO: Integrate class declarations to output file structure
+// TODO: Refactor function declarations into class declarations (as methods)
 // TODO: Add missing tests for declaration files, function declarations and
 // returns
+// TODO: Add method calls
+// TODO: Add properties
 
 private let filePath = CommandLine.arguments[1] + "/tests/"
 
